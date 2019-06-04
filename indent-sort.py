8a9c2594f5a0e99a4e40484fa25e9fcdaff3e0e3
TAAPArthur <taaparthur@gmail.com>
2019-06-04 03:06:36 -0500

    Added option to only sort specified levels

--- indent-sort.py
+++ indent-sort.py
@@ -11,11 +11,23 @@ def getInput():
         yield line
     yield None
 
+sortMinLevel=-1
+sortMaxLevel=float("inf")
+if len(sys.argv) > 1 :
+    parts=sys.argv[1].split("-")
+    if parts:
+        if len(parts)==1:
+            sortMinLevel=sortMaxLevel=int(parts[0])
+        else:
+            if parts[0]:
+                sortMinLevel=int(parts[0])
+            if parts[1]:
+                sortMaxLevel=int(parts[1])
 
 def indentSort():
-    stack = [(-1, [""])]
+    stack = [(-1,-1, [""])]
     for line in getInput():
-        currentLevel, lines = stack[-1]
+        currentLevel,currentNestLevel, lines = stack[-1]
         if line != None:
             trimmedLine = line.lstrip()
 
@@ -24,28 +36,29 @@ def indentSort():
         else:
             indentLevel = -1
         while currentLevel > indentLevel:
-            stack.pop()
+            currentNestLevel= stack.pop()[1]
             if lines:
-                changeComma=False
-                if comma and not lines[-1].endswith(",\n") and lines[0].endswith(",\n"):
-                    lines[-1]=lines[-1][:-1]+",\n"
-                    changeComma=1
-                lines.sort()
-                if comma and changeComma:
-                    lines[-1]=lines[-1][:-2]+"\n"
+                if sortMinLevel <= currentNestLevel <= sortMaxLevel:
+                    changeComma=False
+                    if comma and not lines[-1].endswith(",\n") and lines[0].endswith(",\n"):
+                        lines[-1]=lines[-1][:-1]+",\n"
+                        changeComma=1
+                    lines.sort()
+                    if comma and changeComma:
+                        lines[-1]=lines[-1][:-2]+"\n"
 
-                stack[-1][1][-1] += "".join(lines)
-            currentLevel, lines = stack[-1]
+                stack[-1][-1][-1] += "".join(lines)
+            currentLevel,currentNestLevel, lines = stack[-1]
 
 
         if line == None:
-            return "".join(stack[0][1])
+            return "".join(stack[0][-1])
         if trimmedLine != regex.sub("",line):
-            stack[-1][1][-1] += line
+            stack[-1][-1][-1] += line
         else: 
             if currentLevel != indentLevel:
                 lines = []
-                stack.append((indentLevel, lines))
+                stack.append((indentLevel,currentNestLevel +1, lines))
             lines.append(line)
 
 
