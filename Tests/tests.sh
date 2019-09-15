8e28967ceaa3d229742ae50bf19c5ca19c2ee13f
Arthur Williams <awilliams@purestorage.com>
2019-09-14 19:10:17 -0600

    Changed Testing format
    Automatically detect most tests in Tests/*
    Input file should end in *.orig and the expected result should end in
    *.correct and they should both have the same basename.
    Partial sort tests are still done manually for now.

--- Tests/tests.sh
+++ Tests/tests.sh
@@ -1,6 +1,12 @@
+#!/bin/bash
 set -e
-#diff --strip-trailing-cr -q -y <(cat tests1 | ../indent-sort.py) tests1-correct
-#diff --strip-trailing-cr -q -y <(cat tests2 | ../indent-sort.py) tests2-correct
-diff --strip-trailing-cr -q <(cat test-partial-sort1 | ../indent-sort.py 1) test-partial-sort1-correct
-diff --strip-trailing-cr -q <(cat test-partial-sort2 | ../indent-sort.py 2) test-partial-sort2-correct
-diff --strip-trailing-cr -q <(cat test-partial-sort3 | ../indent-sort.py 1-2) test-partial-sort3-correct
+diff --strip-trailing-cr -q <(cat test-partial-sort1.orig | ../indent-sort.py 1) test-partial-sort1.correct
+diff --strip-trailing-cr -q <(cat test-partial-sort2.orig | ../indent-sort.py 2) test-partial-sort2.correct
+diff --strip-trailing-cr -q <(cat test-partial-sort3.orig | ../indent-sort.py 1-2) test-partial-sort3.correct
+
+while read file; do
+    original=$(basename $file .correct).orig
+    if ! diff --strip-trailing-cr -q --label "computed" <(cat $original | ../indent-sort.py) $file; then
+        diff --strip-trailing-cr -y <(cat $original | ../indent-sort.py) $file
+    fi
+done < <(ls *correct |grep -v partial)
