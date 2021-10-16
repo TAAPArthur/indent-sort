6a3e78b0daedae1ffcc7a5b292919c2206c5ba64
Arthur Williams <taaparthur@gmail.com>
2021-10-15 22:44:13 -0500

    Made the test script POSIX

--- Tests/tests.sh
+++ Tests/tests.sh
@@ -1,22 +1,22 @@
-#!/bin/bash
-set -e
+#!/bin/sh -e
 
 ../indent-sort.py -h >/dev/null
 ../indent-sort.py --help >/dev/null
 ../indent-sort.py -v >/dev/null
 ../indent-sort.py --version >/dev/null
-while read file; do
-    base=$(basename $file .correct)
-    original=$base.orig
-    cmd=$(head -n1 $original)
-    if [ "${cmd::2}" == "#!" ]; then
-        param="${cmd:2}"
+for file in *correct; do
+    base=$(basename "$file" .correct)
+    original="$base.orig"
+    cmd="$(head -n1 "$original")"
+    if [ "${cmd#\#\!}" != "$cmd" ]; then
+        param="${cmd#\#\!}"
     else
-        param=$(echo $base | grep -Po "partial-sort\K.*" || true)
+        param="${base#test-partial-sort}"
+        [ "$param" = "$base" ] && param=0-
     fi
-    if ! diff --strip-trailing-cr -q --label "computed" <(cat $original | ../indent-sort.py $param) $file; then
+    if ! ../indent-sort.py $param  < "$original"| diff -q --label "computed" - "$file"; then
         echo "Param: '$param'"
-        cat $original | ../indent-sort.py $param> $base.wrong
-        diff --strip-trailing-cr --suppress-common-lines -y <(cat $original | ../indent-sort.py $param) $file
+        ../indent-sort.py "$param" < "$original" > "$base.wrong"
+        ../indent-sort.py "$param" < "$original" | diff -u - "$file"
     fi
-done < <(ls *correct)
+done
