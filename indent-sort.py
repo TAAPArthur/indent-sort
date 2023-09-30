84e1ddab49de21b1af29b3e46477fafff5a8c545
Arthur Williams <taaparthur@disroot.org>
2023-09-29 17:22:10 -0700

    Encapsulate non-const globals into a setting class

--- indent-sort.py
+++ indent-sort.py
@@ -12,25 +12,46 @@ startingBlockRegex = re.compile(r'^\s*(\n|#|/\*|//|/\*|<!--|@|template).*')
 modifiersRegex = re.compile(r"(public|static|abstract|private|final|const)\s*")
 continuationRegex = re.compile(r"((,| \\)\n)$")
 
-IGNORE_MODIFIERS = False
-KEY_SPLIT = None
+
+class SortSettings:
+    start_key = 0
+    ignore_modifiers = False
+    ignore_case = False
+    sort_min_level = -1
+    sort_max_level = float("inf")
+
+    def __init__(self, key=0, ignore_modifiers=False, ignore_case=False, sort_range=""):
+        self.start_key = key if isinstance(key, int) else int(key.split(",")[0])
+        self.ignore_modifiers = ignore_modifiers
+        self.ignore_case = ignore_case
+
+        if sort_range:
+            parts = sort_range.split("-")
+            if parts:
+                if len(parts) == 1:
+                    self.sort_min_level = self.sort_max_level = int(parts[0])
+                else:
+                    if parts[0]:
+                        self.sort_min_level = int(parts[0])
+                    if parts[1]:
+                        self.sort_max_level = int(parts[1])
 
 
-def getSortKey(key):
-    if IGNORE_MODIFIERS:
+def getSortKey(key, settings):
+    if settings.ignore_modifiers:
         key = modifiersRegex.sub("", key)
-    if KEY_SPLIT:
-        key = key.split()[KEY_SPLIT:]
-    if IGNORE_CASE:
+    key = key.split()[settings.start_key:]
+    if settings.ignore_case:
         key = key.lower()
     return key
 
 
 class Block:
 
-    def __init__(self, codeBlock, indentLevel, nestedLevel, header=""):
+    def __init__(self, codeBlock, indentLevel, nestedLevel, settings, header=""):
         self.codeBlock = codeBlock
-        self.sortKey = getSortKey(codeBlock)
+        self.settings = settings
+        self.sortKey = getSortKey(codeBlock, settings)
         self.indentLevel = indentLevel
         self.nestedLevel = nestedLevel
         self.children = []
@@ -55,16 +76,16 @@ class Block:
         else:
             self.codeBlock = replacement(self.codeBlock)
 
-    def finalize(self, sortMinLevel, sortMaxLevel):
+    def finalize(self):
         if self.done:
             return
         self.done = True
         if not self.children or self.isHeader:
             return
         for kid in self.children:
-            kid.finalize(sortMinLevel, sortMaxLevel)
-        if sortMinLevel <= self.nestedLevel + 1 <= sortMaxLevel:
-            assert(self.children[-1])
+            kid.finalize()
+        if self.settings.sort_min_level <= self.nestedLevel + 1 <= self.settings.sort_max_level:
+            assert self.children[-1]
             continuationChar = False
             match = continuationRegex.search(self.children[0].getEnd())
             if not continuationRegex.search(self.children[-1].getEnd()) and match:
@@ -73,7 +94,7 @@ class Block:
             self.children.sort()
             if continuationChar:
                 self.children[-1].modifyLastEntry(lambda x: continuationRegex.sub("\n", x))
-            assert(self.children[-1])
+            assert self.children[-1]
 
     def __str__(self):
         s = ""
@@ -91,16 +112,16 @@ class Block:
             for k in kid:
                 yield k
 
-    def process(self, line, indentLevel, sortMinLevel, sortMaxLevel):
+    def process(self, line, indentLevel):
         if self.done:
             return True
         if self.indentLevel >= indentLevel:
-            self.finalize(sortMinLevel, sortMaxLevel)
+            self.finalize()
             return True
         else:
             if self.children:
-                assert(self.children[-1])
-            if not self.children or self.children[-1].process(line, indentLevel, sortMinLevel, sortMaxLevel):
+                assert self.children[-1]
+            if not self.children or self.children[-1].process(line, indentLevel):
                 if self.children and self.children[-1].indentLevel == indentLevel:
                     if endingBlockRegex.match(line):
                         self.children[-1].footer += line
@@ -108,34 +129,11 @@ class Block:
                 if self.children:
                     if self.children[-1].isHeader:
                         header = str(self.children.pop())
-                        self.children.append(
-                            Block(line, indentLevel, self.nestedLevel + 1, header=header))
+                        self.children.append(Block(line, indentLevel, self.nestedLevel + 1, settings=self.settings, header=header))
                         return False
-                self.children.append(
-                    Block(line, indentLevel, self.nestedLevel + 1))
-                assert(self.children[-1])
+                self.children.append(Block(line, indentLevel, self.nestedLevel + 1, settings=self.settings))
+                assert self.children[-1]
         return False
-
-
-def indentSort(sortMinLevel=-1, sortMaxLevel=float("inf")):
-    root = Block("", -1, -1)
-    lastLevel = -1
-    for i, line in enumerate(getInput()):
-        if i == 0 and line and line.startswith("#!"):
-            root = Block(line, -1, -1)
-            continue
-        if line != None:
-            trimmedLine = line.lstrip()
-            indentLevel = len(line) - len(
-                trimmedLine) if trimmedLine else lastLevel
-            if not trimmedLine:
-                indentLevel = lastLevel + 1
-            else:
-                lastLevel = indentLevel
-        else:
-            indentLevel = -1
-        root.process(line, indentLevel, sortMinLevel, sortMaxLevel)
-    return root
 
 
 def getInput():
@@ -144,30 +142,35 @@ def getInput():
     yield None
 
 
+def indentSort(settings):
+    root = Block("", -1, -1, settings=settings)
+    lastLevel = -1
+    for i, line in enumerate(getInput()):
+        if i == 0 and line and line.startswith("#!"):
+            root = Block(line, -1, -1, settings)
+            continue
+        if line != None:
+            trimmedLine = line.lstrip()
+            indentLevel = len(line) - len(trimmedLine) if trimmedLine else lastLevel
+            if not trimmedLine:
+                indentLevel = lastLevel + 1
+            else:
+                lastLevel = indentLevel
+        else:
+            indentLevel = -1
+        root.process(line, indentLevel)
+    return root
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-v", "--version", action="version", version="1.1")
     parser.add_argument("-m", "--ignore-modifiers", default=False, action="store_const", const=True)
-    parser.add_argument("-k", "--key", default=None, type=int, help="Skip the first N words when sorting")
+    parser.add_argument("-k", "--key", default=0, help="Skip the first N words when sorting")
     parser.add_argument("-i", "--ignore-case", default=False, action="store_const", const=True)
-    parser.add_argument("range", default=None, nargs="?")
+    parser.add_argument("sort_range", default=None, nargs="?")
     namespace = parser.parse_args()
-    KEY_SPLIT = namespace.key
-    IGNORE_CASE = namespace.ignore_case
-    IGNORE_MODIFIERS = namespace.ignore_modifiers
+    settings = SortSettings(key=namespace.key, ignore_case=namespace.ignore_case, ignore_modifiers=namespace.ignore_modifiers, sort_range=namespace.sort_range)
 
-    sortMinLevel = -1
-    sortMaxLevel = float("inf")
-    if namespace.range:
-        parts = namespace.range.split("-")
-        if parts:
-            if len(parts) == 1:
-                sortMinLevel = sortMaxLevel = int(parts[0])
-            else:
-                if parts[0]:
-                    sortMinLevel = int(parts[0])
-                if parts[1]:
-                    sortMaxLevel = int(parts[1])
-
-    root = indentSort(sortMinLevel, sortMaxLevel)
+    root = indentSort(settings)
     print(root, end="")
